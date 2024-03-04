05635c4a18dae66e8b3c31391d7e06a77aa76f91
BrentRC <112354813+BrentRC@users.noreply.github.com>
2024-03-04 17:14:45 -0500

    added functionality to main

---
+++ main.py
@@ -0,0 +1,18 @@
+import arcade
+import tester
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 700
+SCREEN_TITLE = "Starting Template"
+ROW_COUNT = 10
+COLUMN_COUNT = 10
+BOARD_RIGHT = 250
+BOARD_LEFT = 200
+BOARD_BOTTOM = 100
+BOARD_TOP = 150
+BOARD_MARGIN = 50
+def main():
+    """ Main function """
+    game = tester.MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
+main()
